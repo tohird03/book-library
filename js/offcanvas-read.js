72e88a8176466fc4101a1fd2f71500cf7a7dc2a8
tohird03 <tohirdoniyorov2003@gamil.com>
2022-02-12 15:01:41 +0500

    finished

--- js/offcanvas-read.js
+++
@@ -1,9 +0,0 @@
-var offcanvasElementList = [].slice.call(document.querySelectorAll('.offcanvas'))
-var offcanvasList = offcanvasElementList.map(function (offcanvasEl) {
-  return new bootstrap.Offcanvas(offcanvasEl)
-})
-
-var myOffcanvas = document.getElementById('myOffcanvas')
-myOffcanvas.addEventListener('hidden.bs.offcanvas', function () {
-  // do something...
-})
